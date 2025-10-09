6c828106e178f9d6244723a61b05b6c6e575b155
Nhlakanipho <nhmasiljhb024@student.wethinkcode.co.za>
2025-10-09 23:16:01 +0200

    Refactor: Improve layout and styling of Hero component for better responsiveness

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -6,12 +6,12 @@ const Hero = () => {
   return (
     <section className="flex flex-col md:flex-row lg:flex-row items-center justify-center min-h-screen overflow-hidden">
       <Illustration />
-      <div className="flex flex-col justify-center w-[70vw] text-center md:text-center lg:text-left">
+      <div className="flex flex-col justify-center items-center md:items-start w-[70vw] text-center md:text-center lg:text-left">
         <h1 className="text-5xl md:text-6xl font-bold text-gray-900 text-center mb-3 tracking-tight">TravelMate</h1>
         <p className="sm:text-[18px] md:text-[20px] lg:text-[20px] text-gray-500 text-center mb-3">Browse Taxi Ranks, Routes and <br/>
           staying safe with real-time Alerts</p>
         <Link href="/signup">
-          <Button className="w-[8rem] bg-[#D08726] text-white my-[1rem]">Get Started!</ Button>
+          <Button className="w-auto px-6 py-3 bg-[#D08726] text-white my-4">Get Started!</Button>
         </Link>
       </div>
 
