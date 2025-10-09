56414b1bcb09c9f0fca127b276cb4c0a28da0831
Nhlakanipho <nhmasiljhb024@student.wethinkcode.co.za>
2025-10-09 23:19:04 +0200

    Refactor: Enhance layout and styling of Hero component for improved readability and responsiveness

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -3,22 +3,22 @@ import Illustration from '@/assets/Illustration'
 import { Button } from '@/components/ui/button'
 import { Link } from 'wouter'
 const Hero = () => {
-  return (
-    <section className="flex flex-col md:flex-row lg:flex-row items-center justify-center min-h-screen overflow-hidden">
-      <Illustration />
-      <div className="flex flex-col justify-center items-center md:items-start w-[70vw] text-center md:text-center lg:text-left">
-        <h1 className="text-5xl md:text-6xl font-bold text-gray-900 text-center mb-3 tracking-tight">TravelMate</h1>
-        <p className="sm:text-[18px] md:text-[20px] lg:text-[20px] text-gray-500 text-center mb-3">Browse Taxi Ranks, Routes and <br/>
-          staying safe with real-time Alerts</p>
-        <Link href="/signup">
-          <Button className="w-auto px-6 py-3 bg-[#D08726] text-white my-4">Get Started!</Button>
-        </Link>
-      </div>
+    return (
+        <section className="flex flex-col md:flex-row lg:flex-row items-center justify-center min-h-screen overflow-hidden">
+            <Illustration />
+            <div className="flex flex-col justify-center w-[70vw] text-center md:text-center lg:text-left">
+                <h1 className="text-6xl font-bold text-gray-900">Travel Mate</h1>
+                <p className="text-2xl text-gray-500">Browse Taxi Ranks, Routes and <br />
+                    staying safe with real-time Alerts</p>
+                <Link href="/signup">
+                    <Button className="w-[8rem] bg-[#D08726] text-white my-[1rem]">Get Started!</ Button>
+                </Link>
+            </div>
 
 
 
-    </section>
-  );
+        </section>
+    );
 };
 
 
